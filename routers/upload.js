1d0dc1e43c0b5e95dd5cf0cdd878e1b612b8dd76
이수혁 <cg10036@isuhyeog-ui-MacBookPro.local>
2020-08-04 12:20:29 +0900

    upload

--- routers/upload.js
+++ routers/upload.js
@@ -2,12 +2,46 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const path = require("path");
+const md5 = require("md5");
 
 router.post("/", (req, res) => {
-  if (!fs.existsSync(path.join(__dirname, "../data/" + req.id))) {
-    fs.mkdirSync(path.join(__dirname, "../data/" + req.id));
+  if (
+    !req.body.opt ||
+    !req.body.opt.num ||
+    isNaN(req.body.opt.num) ||
+    !req.body.data ||
+    !req.body.name
+  ) {
+    return res.status(400).json();
   }
-  res.json({ success: true });
+  let home = path.join(
+    __dirname,
+    "../data/" + Buffer.from(req.id).toString("base64").replace("/", "_")
+  );
+  try {
+    if (!fs.existsSync(home)) {
+      fs.mkdirSync(home);
+    }
+  } catch {
+    return res.status(500).json();
+  }
+  // req.body.opt => {num, overwrite?}
+  let name = Buffer.from(req.body.name).toString("base64").replace("/", "_");
+  if (!fs.existsSync(path.join(home, name + ".FILE"))) {
+    fs.writeFileSync(path.join(home, name + ".FILE"), name, "utf8");
+  }
+  if (
+    fs.existsSync(path.join(home, name + "." + req.body.opt.num.toString())) &&
+    !req.body.opt.overwrite
+  ) {
+    return res.status(409).json();
+  }
+  fs.writeFileSync(
+    path.join(home, name + "." + req.body.opt.num.toString()),
+    req.body.data,
+    "utf8"
+  );
+  res.status(201).json({ hash: md5(req.body.data) });
 });
 
 module.exports = router;
